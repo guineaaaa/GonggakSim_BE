64e072ddbfb105c88ae5fe4e933937671b5c93d7
afflogy <dlatnqls921@naver.com>
2025-01-25 22:07:08 +0900

    [REFACT] 배포 형식 cjs로 수정

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,9 +3,9 @@ import esbuild from "esbuild";
 await esbuild.build({
     entryPoints: ["./src/index.ts"],
     bundle: true,
-    format: "esm",
+    format: "cjs",
     outdir: "./dist",
-    outExtension: { ".js": ".mjs" },
+    outExtension: { ".js": ".cjs" },
     platform: "node",
     target: "node20",
     minify: true,
