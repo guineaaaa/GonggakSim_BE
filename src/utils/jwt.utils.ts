37e1d16eae2b0b6aa4651b06c7b3cb25b525dff5
afflogy <dlatnqls921@naver.com>
2025-02-09 19:54:13 +0900

    [REFACT] jwtToken 만료시간 수정

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -4,7 +4,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 // payload에 원하는 사용자 정보를 담아 토큰 생성 (예: id, email, provider 등)
 export const generateJWTToken = (payload: object): string => {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: '100y' }); // 개발과정이라 만료시간을 100년 설정, 기본 1h
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: '7d' }); // 개발과정이라 만료시간을 100년 설정, 기본 1h
 };
 
 // JWT 검증 함수
