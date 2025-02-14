9c329cc401976d856f6dc6b6ad398a6d8a2e732b
afflogy <dlatnqls921@naver.com>
2025-02-14 15:51:05 +0900

    [REFACT] 개발용 로그인 환경 설정

---
+++ src/dtos/auth.dto.ts
@@ -0,0 +1,12 @@
+// 로그인 dto
+export interface LoginDTO {
+    email: string;
+    password: string;
+  }
+  
+  export interface LoginResponseDTO {
+    id: number;
+    email: string;
+    name: string;
+    token: string;
+  }
